f6b340a076e31d8f2e518639444ae2935c740344
Delian Petrov <delian7@gmail.com>
2024-09-28 21:03:35 +0000

    coming soon text

--- delian7/src/App.tsx
+++ delian7/src/App.tsx
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          Delian Petrov's React Website. Coming Soon!
         </p>
         <a
           className="App-link"
