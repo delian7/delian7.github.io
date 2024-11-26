036df0c8f51a672b49d99d534bb0ac9f7e1f3ae9
Delian Petrov <delian@fridgeguideai.com>
2024-11-25 18:03:30 -0800

    styling the seastatus modal

--- delian7/src/components/SeastatusModalContent.tsx
+++ delian7/src/components/SeastatusModalContent.tsx
@@ -1,4 +1,6 @@
 import PhoneFrame from './PhoneFrame';
+import hero from '../images/seastatus/hero.png';
+import { Box } from '@chakra-ui/react';
 
 const SeastatusModalContent = () => {
     const seastatus = require('../images/seastatus/seastatus.mp4');
@@ -6,15 +8,62 @@ const SeastatusModalContent = () => {
     return (
       <div className="modal">
         <div className="modal-content">
-          <div className="column left">
-            <h2>Title</h2>
-            <p>Some text for the left column content. This could include an introduction or general information.</p>
-            <PhoneFrame video={seastatus} />
-          </div>
-          <div className="column right">
-            <h2>Another Title</h2>
-            <p>Text for the right column content. This could include technical details, links, or images.</p>
-          </div>
+          <Box
+            className="row project-image"
+            mb={4}
+            display="flex"
+            flexDirection={{ base: 'column', md: 'row' }} // Responsive flex direction
+            alignItems="center" // Center items vertically
+          >
+            <img src={hero} alt="Seastatus" />
+          </Box>
+          <Box
+            className="row"
+            display="flex"
+            flexDirection={{ base: 'column', md: 'row' }} // Responsive flex direction
+          >
+            <Box
+              className="column"
+              flex={3}
+              borderRight={{base: '0px', md: "1px solid #ccc"}}
+            >
+              <h2>Background</h2>
+              <p>
+                We developed a Ruby on Rails application delivering personalized marine weather data for SeaStatus, taking it from concept to MVP in just two months. The platform aggregates weather data from APIs like NOAA, Dark Sky, and PlanetOS, using Regex for time zone and location-based parsing. We utilized a JavaScript cross-platform framework (Ionic) to efficiently build iOS, Android, and web apps from a single codebase.
+              </p>
+
+              <h2>Challenge</h2>
+              <p>
+                As a pre-seed startup, SeaStatus required a cost-effective, resource-efficient solution. We leveraged open-source libraries and frameworks, used cross-platform development to streamline processes, and relied on third-party APIs for rapid data integration and personalized weather updates.
+              </p>
+
+              <h2>Solution</h2>
+              <p>
+                Aggregated data from NOAA, Dark Sky, PlanetOS, WeatherFlow, and WorldTides into marine weather charts.
+                Cached location data with Amazon CloudFront for fast loading.
+                Built a Ruby on Rails backend on Google Cloud to fetch and store data in Firestore via delayed jobs.
+                Scheduled personalized push notifications using Node.js cloud functions.
+              </p>
+
+              <h2>Success</h2>
+              <p>
+                We launched the SeaStatus MVP in 2 months of development time,
+                and won a $25,000 prize from the Big Ocean Button Challenge!
+                SeaStatus was acquired by Sofar Ocean Technologies for a successful
+                startup exit!
+              </p>
+            </Box>
+            <Box
+              className="column right"
+              flex={2}
+            >
+              <h2>Tech Used</h2>
+              <p>Ruby on Rails, Google Cloud, Firebase, Ionic Framework</p>
+              <Box mt={4}>
+                <PhoneFrame video={seastatus} />
+              </Box>
+            </Box>
+          </Box>
         </div>
       </div>
     );
