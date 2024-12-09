0cd6c554320bcfc5356d5459dd13263ffec49e13
Delian Petrov <delian@fridgeguideai.com>
2024-12-09 22:56:01 +0200

    get rid of the /

--- delian7/src/components/RedirectPage.tsx
+++ delian7/src/components/RedirectPage.tsx
@@ -16,8 +16,7 @@ const RedirectPage = () => {
     const fetchAndRedirect = async () => {
       try {
         // Replace with your Lambda API URL
-        debugger
-        alert("asdf");
+        alert(name);
         const response = await fetch(`https://qpqyy5wg42qcon34ph6mhljct40wtmpl.lambda-url.us-east-2.on.aws/?name=${name}`)
         if (response.ok) {
           const data = await response.json();
