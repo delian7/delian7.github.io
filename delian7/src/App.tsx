c2631b2cbbdc08ada861e2089f028d4dbf39ffbe
Delian Petrov <delian@fridgeguideai.com>
2025-01-07 13:01:11 -0800

    delete the ga package

--- delian7/src/App.tsx
+++ delian7/src/App.tsx
@@ -2,7 +2,7 @@ import { ChakraProvider } from "@chakra-ui/react";
 import Header from "./components/Header";
 import LandingSection from "./components/LandingSection";
 import ProjectsSection from "./components/ProjectsSection";
-import { Route, Routes, HashRouter, useLocation } from 'react-router-dom';
+import { Route, Routes, HashRouter } from 'react-router-dom';
 import ContactMeSection from "./components/ContactMeSection";
 import Timeline from "./components/Timeline";
 import Footer from "./components/Footer";
@@ -11,10 +11,6 @@ import Alert from "./components/Alert";
 import RedirectPage from "./components/RedirectPage";
 import SeastatusModalContent from "./components/SeastatusModalContent";
 import { ModalProvider } from "./context/modalContext";
-import ReactGA from 'react-ga4';
-import { useEffect } from 'react';
-const TRACKING_ID = 'G-5B1WCC02QY'; // Replace with your GA4 Measurement ID
-ReactGA.initialize(TRACKING_ID);
 
 const HomePage = () => {
   return (
@@ -31,12 +27,6 @@ const HomePage = () => {
 }
 
 function App() {
-  const location = useLocation();
-
-  useEffect(() => {
-    ReactGA.send({ hitType: 'pageview', page: location.pathname });
-  }, [location]);
-
   return (
     <HashRouter>
       <ChakraProvider>
