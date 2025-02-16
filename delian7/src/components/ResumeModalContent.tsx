00cc81f9d7474deb96b8950494ace9a9b6213b3e
Delian Petrov <delian@fridgeguideai.com>
2025-02-16 10:12:47 -0800

    updates to the resume

--- delian7/src/components/ResumeModalContent.tsx
+++ delian7/src/components/ResumeModalContent.tsx
@@ -1,15 +1,22 @@
 import { Box, Button } from "@chakra-ui/react";
 
 const ResumeModalContent = () => {
+    const pdfID = "1xl2LsWDi7Soc18Jh-fdSg49eDg39NkPA"; // Define your PDF ID
+    const docxID = "1bIkRhZl2OXN-WMPDaxurv8nsTZCfiI21"; // Define your DOCX ID
+
     return (
       <Box height="900px" display={'flex'} flexDirection={'column'}>
         <Box color={'green.400'} marginY={'8'} display={'flex'} justifyContent={'space-evenly'}>
-          <Button colorScheme="blue" variant='solid'><a target="_blank" rel="noreferrer" href="https://drive.google.com/uc?export=download&id=1xl2LsWDi7Soc18Jh-fdSg49eDg39NkPA">Download in PDF</a></Button>
-          <Button colorScheme="blue" variant='solid'><a target="_blank" rel="noreferrer" href="https://drive.google.com/uc?export=download&id=17SXDG-HRIgA8amhI0CbH0b8mhfz5ZcJP">Download in DOCX</a></Button>
+          <Button colorScheme="blue" variant='solid'>
+            <a target="_blank" rel="noreferrer" href={`https://drive.google.com/file/d/${pdfID}/preview`}>Download in PDF</a>
+          </Button>
+          <Button colorScheme="blue" variant='solid'>
+            <a target="_blank" rel="noreferrer" href={`https://drive.google.com/uc?export=download&id=${docxID}`}>Download in DOCX</a>
+          </Button>
         </Box>
         <iframe
           title='Delian Petrov Resume'
-          src="https://drive.google.com/file/d/1xl2LsWDi7Soc18Jh-fdSg49eDg39NkPA/preview"
+          src={`https://drive.google.com/file/d/${docxID}/preview`}
           width="100%"
           height="100%"
           allow="autoplay"
