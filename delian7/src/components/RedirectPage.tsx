506d0ac11edf643a43c9e1647f97443e034bc31e
Delian Petrov <delian@fridgeguideai.com>
2025-02-21 12:55:28 -0800

    Revert "quick test to revert"
    This reverts commit 9f02ad513c227d93c19f47eeca8077162cfd195c.

--- delian7/src/components/RedirectPage.tsx
+++ delian7/src/components/RedirectPage.tsx
@@ -10,58 +10,51 @@ const RedirectPage = () => {
   const { onOpen } = useAlertContext();
 
 
-  // useEffect(() => {
-  //   const fetchAndRedirect = async () => {
-  //     try {
-  //       // Replace with your Lambda API URL
-  //       const response = await fetch(`https://qpqyy5wg42qcon34ph6mhljct40wtmpl.lambda-url.us-east-2.on.aws/?name=${name}`)
-  //       if (response.ok) {
-  //         const data = await response.json();
-
-  //         if (data.url) {
-  //           const title = name || 'Delian Petrov';
-  //           document.title = title
-  //           document.querySelector('meta[property="og:title"]')?.setAttribute('content', title);
-
-  //           window.location.href = data.url; // Perform the redirect
-
-  //         } else {
-  //           navigate('/'); // Redirect to a 404 page if the name is not found
-  //           onOpen("error", "The short link was not found")
-  //         }
-  //       } else {
-  //         navigate('/'); // Redirect to a 404 page if the name is not found
-  //         onOpen("error", "The short link was not found")
-  //       }
-  //     } catch (error) {
-  //       console.error('Error fetching URL:', error);
-  //       navigate('/'); // Redirect to a generic error page
-  //     }
-  //   };
-
-  //   fetchAndRedirect();
-  // }, [name, navigate, onOpen]);
-
-
   useEffect(() => {
-    window.location.href = `https://delianpetrov.com/api/og?name=${name}`;
-  }, [name]);
+    const fetchAndRedirect = async () => {
+      try {
+        // Replace with your Lambda API URL
+        const response = await fetch(`https://qpqyy5wg42qcon34ph6mhljct40wtmpl.lambda-url.us-east-2.on.aws/?name=${name}`)
+        if (response.ok) {
+          const data = await response.json();
 
-  // return (
-  //   <>
-  //     <Box
-  //       display="flex"
-  //       justifyContent="center"
-  //       alignItems="center"
-  //       minHeight="100vh"
-  //       background={'lavender'}
-  //     >
-  //       <Spinner size={'xl'}></Spinner>
-  //     </Box>
-  //   </>
-  // )
+          if (data.url) {
+            const title = name || 'Delian Petrov';
+            document.title = title
+            document.querySelector('meta[property="og:title"]')?.setAttribute('content', title);
 
-  return null;
+            window.location.href = data.url; // Perform the redirect
+
+          } else {
+            navigate('/'); // Redirect to a 404 page if the name is not found
+            onOpen("error", "The short link was not found")
+          }
+        } else {
+          navigate('/'); // Redirect to a 404 page if the name is not found
+          onOpen("error", "The short link was not found")
+        }
+      } catch (error) {
+        console.error('Error fetching URL:', error);
+        navigate('/'); // Redirect to a generic error page
+      }
+    };
+
+    fetchAndRedirect();
+  }, [name, navigate, onOpen]);
+
+  return (
+    <>
+      <Box
+        display="flex"
+        justifyContent="center"
+        alignItems="center"
+        minHeight="100vh"
+        background={'lavender'}
+      >
+        <Spinner size={'xl'}></Spinner>
+      </Box>
+    </>
+  )
 };
 
 export default RedirectPage;
